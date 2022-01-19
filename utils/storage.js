0e05c8a96aa842674081a313e46ba99f6e5ff119
YiJio <841312998@qq.com>
2022-01-20 07:07:45 +0800

    electron storage 配置调整

--- utils/storage.js
+++ utils/storage.js
@@ -1,11 +1,14 @@
 const path = require('path')
 const storage = require('electron-localstorage')
+const {remote} = require('electron')
 
+console.log(process.env.NODE_ENV)
 let cachePath = ''
 if (process.env.NODE_ENV === 'production') {
   cachePath = path.join(__dirname, '../localConfig-dev.json')
 } else {
-  cachePath = path.join(__dirname, '../localConfig.json')
+  const configDir = remote.app.getPath('userData')
+  cachePath = path.join(configDir, './localConfig.json')
 }
 storage.setStoragePath(cachePath)
 
