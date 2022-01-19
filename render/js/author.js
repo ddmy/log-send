f51a614c52a80b57cc274531707ea3618561396b
YiJio <841312998@qq.com>
2022-01-20 05:17:44 +0800

    日报发送配置优化

--- render/js/author.js
+++ render/js/author.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const nodemailer = require('nodemailer')
 const storage = require(path.join(__dirname, 'utils/storage.js'))
 
 function regTestEmail(str) {
@@ -19,7 +20,7 @@ window.addEventListener('DOMContentLoaded', () => {
     pwd.disabled = false
   }
 
-  document.querySelector('button').addEventListener('click', () => {
+  document.querySelector('.save').addEventListener('click', () => {
     const emailVal = email.value || null
     const pwdVal = pwd.value || null
     const toEmailVal = toEmail.value || null
@@ -33,7 +34,50 @@ window.addEventListener('DOMContentLoaded', () => {
       pwd: pwdVal,
       toEmail: toEmailVal
     }))
-    window.opener = null
-    window.close()
+    // window.opener = null
+    // window.close()
+    alert('保存成功！')
+  })
+  document.querySelector('.test').addEventListener('click', () => {
+    let userInfo = storage.getItem('author') || null
+    if (!userInfo) return alert('日报配置未生效，请保存！')
+    sendEmail(userInfo)
+  })
+  document.querySelector('.set').addEventListener('click', () => {
+    email.disabled = false
+    pwd.disabled = false
   })
 })
+
+function sendEmail (userInfo) {
+  let transporter = nodemailer.createTransport({
+    // host: 'smtp.ethereal.email',
+    service: 'QQ', // 使用了内置传输发送邮件 查看支持列表：https://nodemailer.com/smtp/well-known/
+    port: 465, // SMTP 端口
+    secureConnection: true, // 使用了 SSL
+    auth: {
+      user: userInfo.email,
+      // 这里密码不是qq密码，是你设置的smtp授权码
+      pass: userInfo.pwd,
+    }
+  });
+
+  let name = '测试姓名'
+
+  let mailOptions = {
+    from: userInfo.email, // sender address
+    to: userInfo.toEmail, // list of receivers
+    subject: name + '-' + new Date().toLocaleDateString() + ' 日报', // Subject line
+    html: '测试内容，如果您收到此封邮件，说明邮箱配置正确，可以正常收发邮件!' // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      alert(`${error}`)
+      return
+    }
+    alert('测试邮件已发送，请检查邮箱!')
+    // Message sent: <04ec7731-cc68-1ef6-303c-61b0f796b78f@qq.com>
+  })
+}
